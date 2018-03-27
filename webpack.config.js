b92416f55b0b075ba25621caf6626096727b83ae
mashagrand4 <mashagrand4@gmail.com>
2018-03-27 21:36:04 +0300

    nodejs_uploader/feature: add webpack cli

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
-    },
-    module: {
-        loaders: {
-
-        }
     }
+    // module: {
+    //     loaders: {
+    //
+    //     }
+    // }
 };
