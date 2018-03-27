9e7417097c87486e8be7eed9b55f62aefc6a8772
mashagrand4 <mashagrand4@gmail.com>
2018-03-27 17:34:33 +0300

    added .idea folder to .gitignore

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var fs = require('fs');
 var mongoose = require("mongoose");
 var cookieParser = require('cookie-parser');
 var handlebars = require('handlebars-engine');
+var user = require('./src/DB/DBsettings');
+var router = require('./src/routs/routers');
 
 var app = express();
-
-var router = require('./src/routs/routers')(express, app);
 
 app.engine('hbs', handlebars);
 app.set('views', __dirname + '/views');
@@ -20,6 +20,8 @@ app.use(express.static(path.join(__dirname, 'media')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(router);
+app.use(user);
 
 port = 3000;
 app.listen(port);
