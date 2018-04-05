c214b7041079bc6c05f1fff231051a9826c262a0
Mary <mashagrand4@gmail.com>
2018-04-05 19:22:09 +0300

    Fixed window.load

--- src/frontend/js/imagesPreview.js
+++ src/frontend/js/imagesPreview.js
@@ -2,13 +2,12 @@ import '../css/style.css';
 import '../css/form.css';
 import template from '../previewTemplate.hbs';
 
-
 let fileList = {};
 let filesArr = [];
 
 const isValidFile = fileItem => /\.(jpe?g|png|pdf)$/i.test(fileItem.name) && fileItem.size < 5242880;
 
-const readAndPreview = (fileItem) => {
+const addPreview = (fileItem) => {
   if (isValidFile(fileItem)) {
     const previewBlock = document.querySelector('#previews');
     previewBlock.innerHTML += template({ src: URL.createObjectURL(fileItem) });
@@ -23,45 +22,45 @@ const updateFileList = () => {
   fileList = Object.assign({}, filesArr);
 };
 
-const delPrev = (event) => {
+const deleteImage = (event) => {
   objToArr(fileList);
   filesArr = filesArr.filter(fileItem => fileItem.name !== event.target.parentNode.dataset.nameImg);
   event.target.parentNode.parentNode.removeChild(event.target.parentNode);
   updateFileList();
 };
 
-window.onload = () => {
-  document.querySelector('#upload').addEventListener('click', (e) => {
-    e.preventDefault();
-    const formData = new FormData();
+window.addEventListener('load',() => {
+    document.querySelector('#upload').addEventListener('click', (e) => {
+        e.preventDefault();
+        const formData = new FormData();
 
-    if (fileList) {
-      for (let i = 0; i < Object.keys(fileList).length; i += 1) {
-        formData.append('files', fileList[i]);
-      }
-    }
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/upload', true);
-    xhr.send(formData);
+        if (fileList) {
+            for (let i = 0; i < Object.keys(fileList).length; i += 1) {
+                formData.append('files', fileList[i]);
+            }
+        }
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/upload', true);
+        xhr.send(formData);
 
-    fileList = {};
-    filesArr = [];
-    document.querySelector('#previews').innerHTML = '';
-  });
+        fileList = {};
+        filesArr = [];
+        document.querySelector('#previews').innerHTML = '';
+    });
 
-  document.querySelector('#previews').addEventListener('click', (e) => {
-    if (fileList) {
-      delPrev(e);
-    }
-  });
+    document.querySelector('#previews').addEventListener('click', (e) => {
+        if (fileList) {
+            deleteImage(e);
+        }
+    });
 
-  document.querySelector('#file_add').addEventListener('change', (e) => {
-    e.preventDefault();
-    if (fileList) {
-      [].forEach.call(document.querySelector('input[type=file]').files, readAndPreview);
-    }
-    const tmpFileList = objToArr(document.querySelector('input[type=file]').files);
-    filesArr = filesArr.concat(tmpFileList);
-    updateFileList(filesArr);
-  });
-};
+    document.querySelector('#file_add').addEventListener('change', (e) => {
+        e.preventDefault();
+        if (fileList) {
+            [].forEach.call(document.querySelector('input[type=file]').files, addPreview);
+        }
+        const tmpFileList = objToArr(document.querySelector('input[type=file]').files);
+        filesArr = filesArr.concat(tmpFileList);
+        updateFileList(filesArr);
+    });
+});
--- src/frontend/js/validation.js
+++ src/frontend/js/validation.js
@@ -2,18 +2,18 @@ import '../css/style.css';
 import '../css/form.css';
 import '../images/logo_big.png';
 
-// window.onload = () => {
-//   const password = document.getElementById('password');
-//   const confirmPassword = document.getElementById('confirm_password');
-//
-//   const validatePassword = () => {
-//     if (password.value !== confirmPassword.value) {
-//       confirmPassword.setCustomValidity("Passwords Don't Match");
-//     } else {
-//       confirmPassword.setCustomValidity('');
-//     }
-//   };
-//
-//   password.onchange = validatePassword;
-//   confirmPassword.onkeyup = validatePassword;
-// };
+window.addEventListener('load',() => {
+  const password = document.getElementById('password');
+  const confirmPassword = document.getElementById('confirm_password');
+
+  const validatePassword = () => {
+    if (password.value !== confirmPassword.value) {
+      confirmPassword.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirmPassword.setCustomValidity('');
+    }
+  };
+
+  password.onchange = validatePassword;
+  confirmPassword.onkeyup = validatePassword;
+});
