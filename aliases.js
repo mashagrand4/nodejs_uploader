09c7a5e6899225c247c4106c0e5ab4cd606842a4
mashagrand4 <mashagrand4@gmail.com>
2018-04-09 14:31:06 +0300

    Fixed code duplication

--- aliases.js
+++ aliases.js
@@ -1,16 +1,13 @@
 import path from 'path';
 import moduleAlias from 'module-alias';
 
-export default {
+const paths = {
   '@root': path.resolve(__dirname, '.'),
   '@pages': path.resolve(__dirname, 'src/backend/views'),
   '@frontend': path.resolve(__dirname, 'src/frontend'),
   '@backend': path.resolve(__dirname, 'src/backend'),
 };
 
-moduleAlias.addAlias({
-  '@root': path.resolve(__dirname, '.'),
-  '@pages': path.resolve(__dirname, 'src/backend/views'),
-  '@frontend': path.resolve(__dirname, 'src/frontend'),
-  '@backend': path.resolve(__dirname, 'src/backend'),
-});
+moduleAlias.addAlias(paths);
+
+export default paths;
