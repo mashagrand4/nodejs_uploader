2aeb47da82afd98d7b39dfb8c62612fc34b17651
Maria Mashinskaya <Mashinskayam@VRP.local>
2018-04-23 15:00:17 +0300

    Fixed eslint

--- src/frontend/js/services/tests/range.spec.js
+++ src/frontend/js/services/tests/range.spec.js
@@ -13,15 +13,14 @@ test('type of value should be a number', () => {
 });
 
 test('if type of value is null/undefined to throw exception', () => {
-    expect(() => range(undefined)).toThrow('value must be a number');
+  expect(() => range(undefined)).toThrow('value must be a number');
 });
 
 test('if type of value is null/undefined to throw exception', () => {
-    expect(() => range(null)).toThrow('value must be a number');
+  expect(() => range(null)).toThrow('value must be a number');
 });
 
 test('if min/max is number and value is between their to equal true', () => {
-    expect(range(5242879, 23, 10000000000)).toBe(true);
+  expect(range(5242879, 23, 10000000000)).toBe(true);
 });
-
 
