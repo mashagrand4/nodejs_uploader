d2404d5f26b5006612530d1fd0e37ed39a694d07
Maria Mashinskaya <Mashinskayam@VRP.local>
2018-04-23 14:42:54 +0300

    Added new tests and fixed description for all tests

--- src/frontend/js/services/tests/range.spec.js
+++ src/frontend/js/services/tests/range.spec.js
@@ -8,7 +8,20 @@ test('should be equal true', () => {
   expect(range(5242881)).toBe(false);
 });
 
-test('value should be a number', () => {
+test('type of value should be a number', () => {
   expect(() => range('ddf')).toThrow('value must be a number');
 });
+
+test('if type of value is null/undefined to throw exception', () => {
+    expect(() => range(undefined)).toThrow('value must be a number');
+});
+
+test('if type of value is null/undefined to throw exception', () => {
+    expect(() => range(null)).toThrow('value must be a number');
+});
+
+test('if min/max is number and value is between their to equal true', () => {
+    expect(range(5242879, 23, 10000000000)).toBe(true);
+});
+
 
